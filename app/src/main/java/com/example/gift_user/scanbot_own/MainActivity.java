36afa587b73aa1f4402272ed7f09af230d8daeaa
kamaljee7 <kamaliskingmaker@gmail.com>
2017-11-23 11:41:14 +0530

    first commit

---
+++ app/src/main/java/com/example/gift_user/scanbot_own/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gift_user.scanbot_own;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
